883dc7f49d98bdae5cc6b7c5c13bfc30d2d30cda
2zoo99 <20234120@gm.hannam.ac.kr>
2024-04-24 18:18:10 +0900

    조건문 동작 확인

---
+++ example02.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+    int m=0;
+    for(int i=5; i<3; i++){
+        m++;
+    }
+    if(m==1){printf("if result is false execute one time");}
+    else {printf("no execute"); }
+
+    return 0;
+}
