c81381d8df215b0a7baa8ba6f3edc83e25353d50
2zoo99 <20234120@gm.hannam.ac.kr>
2024-04-25 18:45:19 +0900

    조건문 동작 확인 + bool 출력 확인

--- example02.c
+++ example02.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main() {
 
@@ -9,5 +10,13 @@ int main() {
     if(m==1){printf("if result is false execute one time");}
     else {printf("no execute"); }
 
+    printf("\n");
+    bool x = true;
+    bool y = false;
+    printf("%d", x&x); //1
+    printf("%d", x&y); //0
+    printf("%d", x|x); //1
+    printf("%d", x|y); //1
+    printf("%d", y|y); //0
     return 0;
 }
